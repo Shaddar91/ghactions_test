b0e6b4e53359e1fee83ad857367a2d8d7a4a23cf
My Name <shaddar45@gmail.com>
2022-09-08 15:55:15 +0200

    testing

--- terraform/modules/github/create_repo/new/gh_actions.tf
+++ terraform/modules/github/create_repo/new/gh_actions.tf
@@ -4,13 +4,13 @@
 #   selected_repository_ids = [github_repository.main.repo_id]
 # }
 
-# resource "github_repository_file" "gh_actions_master" {
-#   repository          = github_repository.main.name
-#   branch              = "master"
-#   file                = "git/workflow/main.yml"
-#   content             = file("files/master_actions/main.yml")
-#   commit_message      = "Managed by Terraform"
-#   commit_author       = "Terraform User"
-#   commit_email        = "terraform@terraform.com"
-#   overwrite_on_create = var.overwrite_on_create
-# }
+resource "github_repository_file" "gh_actions_master" {
+  repository          = github_repository.main.name
+  branch              = "master"
+  file                = ".github/workflow/main.yml"
+  content             = file("files/master_actions/main.yml")
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "terraform@terraform.com"
+  overwrite_on_create = var.overwrite_on_create
+}
